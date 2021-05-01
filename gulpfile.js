5c792c7f40a27ec74c52ac30b47a8809eb530814
igorek19882020 <igorek20081988@mail.ru>
2021-05-01 15:49:28 +0300

    add section slider

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,9 @@ gulp.task('sass', function(){
     return gulp.src([
       'node_modules/normalize.css/normalize.css',
       'node_modules/slick-carousel/slick/slick.css',
-      'node_modules/magnific-popup/dist/magnific-popup.css'])
+      'node_modules/magnific-popup/dist/magnific-popup.css',
+      'node_modules/rateyo/ry/rateyo/jquery.rateyo.css'])
+    
       .pipe(concat('libs.min.css'))
       .pipe(cssmin())
       .pipe(gulp.dest('app/css'))
@@ -30,10 +32,9 @@ gulp.task('sass', function(){
       return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/ry/rateyo/jquery.rateyo.js',
       ])
-        
-        
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('app/js'))
